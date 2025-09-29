105cf4f89b4cd5dc14e275df8f362dcc53aa72fb
EditHive <anuragsonawane2006@gmail.com>
2025-09-29 17:14:01 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'EditHive',
+  description: 'Professional Video Editing agency',
   generator: 'v0.dev',
 }
 
